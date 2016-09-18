01a707d92f40c4408bb6ddfcb6d3b98b3e2dc513
Zzzkvidi4 <zzzkvidi4@gmail.com>
2016-09-18 18:05:10 +0300

    Now users' group params are shown

---
+++ src/AppBundle/DataClasses/UserEdition.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Роман
+ * Date: 18.09.2016
+ * Time: 14:29
+ */
+
+namespace AppBundle\DataClasses;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use AppBundle\Validator\Constraints as MyAssert;
+
+
+class UserEdition
+{
+    /**
+     *      @Assert\NotBlank(message="Значение не может быть пустым."),
+     *      @Assert\Type(
+     *          type="digit",
+     *          message="Значение не может быть не числом.")
+     *      @CheckAssert\CheckUserID()
+     *      @Assert\NotNull(message = "Значение не может быть нулевым")
+     */
+    protected $user_id;
+
+
+    /**
+     *      @Assert\NotBlank(message="Значение не может быть пустым."),
+     *      @Assert\Type(
+     *          type="digit",
+     *          message="Значение не может быть не числом.")
+     *      @CheckAssert\CheckUserGroupID()
+     *      @Assert\NotNull(message = "Значение не может быть нулевым")
+     */
+    protected $user_group_id;
+
+    /**
+     * @return mixed
+     */
+    public function getUserGroupId()
+    {
+        return $this->user_group_id;
+    }
+
+    /**
+     * @param mixed $user_group_id
+     * @return $this
+     */
+    public function setUserGroupId($user_group_id)
+    {
+        $this->user_group_id = $user_group_id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param mixed $user_id
+     * @return $this
+     */
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+        return $this;
+    }
+}
