faee70c8609253bd2e2636c3080f9ebc4acc3552
Nef1k <nef1k@outlook.com>
2016-09-21 15:37:02 +0300

    Refactored YesNoDialog

--- web/js/admin_script.js
+++ web/js/admin_script.js
@@ -2,47 +2,31 @@
  * Created by ASUS on 20.09.2016.
  */
 
-function showYesNoDialog(modal_params){
-    $("#modal-caption").html(modal_params.caption);
-    $("#modal-message").html(modal_params.message);
-    $("#modal-yes-btn").click(modal_params.yesHandler);
-    $("#modal-no-btn").click(modal_params.noHandler);
-
-    if (modal_params.yes_caption){
-        $("#modal-yes-btn").html(modal_params.yes_caption);
-    }
-
-    if (modal_params.no_caption){
-        $("#modal-no-btn").html(modal_params.no_caption);
-    }
-
-    $("#yes-no-modal").modal({
-        show: true
-    });
-}
-
-function closeYesNoModal(){
-    $("#yes-no-dialog").modal({
-        show: false
-    });
-}
-
 function createModalNoBtn(event){
     closeYesNoModal();
 }
-
 function createModalYesBtn(event) {
     closeYesNoModal();
-    alert("Туц");
+}
+
+function getCertificateListCaption(event){
+
+}
+
+function getCertificatesCaption(event){
+    var modal = $(this);
+
+
 }
 
 function createBtnClick(){
-    showYesNoDialog({
+    var yesNoDialog = new YesNoDialog();
+    yesNoDialog.show({
         caption: "Создание сертификатов",
         message: "Будут созданы сертификаты %certificates%. Вы уверены?",
 
-        yesCaption: "Да",
-        noCaption: "Нет",
+        yes_caption: "Да",
+        no_caption: "Нет",
 
         yesHandler: createModalYesBtn,
         noHandler: createModalNoBtn
