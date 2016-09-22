e9fd52313f8b0565fa438774cf92165b49a3db07
Zzzkvidi4 <zzzkvidi4@gmail.com>
2016-09-22 11:13:02 +0300

    Modify creation certs

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -195,9 +195,8 @@ class AdminController extends Controller{
                     setIDSertificate($cert_id)->
                     setIDSertState($cert_state)->
                     setIDUser($user);
-                $em->persist($cert);
+                $em->flush($user);
             }
-            $em->flush();
             array_push($Request_output, 'success');
         }
         foreach($errors as $error){
