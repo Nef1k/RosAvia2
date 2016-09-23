7c30ff1c1fdaa3fece0ecb3b0c0eede7f2402392
Nef1k <nef1k@outlook.com>
2016-09-23 17:15:09 +0300

    Working on certificate attachment

--- web/js/Stuff/CertificateList.js
+++ web/js/Stuff/CertificateList.js
@@ -2,20 +2,20 @@
  * Created by Nef1k on 18.09.2016.
  */
 
-CertificateList = {
-    __certificate_list: [],
+function CertificateList(){
+    this.__certificate_list = [];
 
-    add: function(id) {
+    this.add = function(id) {
         var indexToAdd = this.__certificate_list.indexOf(id);
         if (indexToAdd < 0) {
             this.__certificate_list.push(id);
         }
-    },
+    };
 
-    remove: function(id) {
+    this.remove = function(id) {
         var indexToDelete = this.__certificate_list.indexOf(id);
         if (indexToDelete >= 0) {
             this.__certificate_list = this.__certificate_list.splice(indexToDelete, 1);
         }
-    }
-};
+    };
+}
