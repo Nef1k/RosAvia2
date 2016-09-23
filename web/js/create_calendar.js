0beb0c73413f30ba8e5ea2c035a1394c05e02db9
Nef1k <nef1k@outlook.com>
2016-09-23 11:08:20 +0300

    Implemented per-day schedule rendering

--- web/js/create_calendar.js
+++ web/js/create_calendar.js
@@ -1,63 +1,57 @@
 /**
  * Created by 166878 on 20.09.2016.
  */
-function showModal(date){
-    $("#chosen_date").html(date.format("DD.MM.YYYY"));
-    $("#time_table .hour-row").remove();
-    $("#myModal").modal("show");
-    $(".day-empty").addClass("hidden");
+function certificateToStr(certificate){
+    return  "<a href='#' class='btn btn-primary' style='margin-bottom: 5px;'>" +
+            "   <span class='badge'>" + certificate.id + "</span>" +
+            "   " + certificate.flight_type +
+            "</a> ";
+}
+function getHourRow(hour){
+    return  "<tr class='hour-row'>" +
+            "   <td valign='middle' align='center'>" + hour + ":00</td>" +
+            "   <td style='padding: 10px 20px;'>" +
+            "       <div class='row certificates-in-hour-" + hour + "'></div>" +
+            "   </td>" +
+            "</tr>";
+}
 
+function showModal(date){
+    //Showing modal window with per-day schedule table
+    $("#chosen_date").html(date.format("DD.MM.YYYY"));
+    $("#time_tabl").find(".hour-row").remove();
+    $(".day-empty").addClass("hidden");
+    $("#myModal").modal("show");
+
+    //Retrieving per-day schedule
     var chosen_date = date.unix();
     getTimeTableData(chosen_date);
 }
 function fillTimeTableWithData(table_selector, data){
-    console.log(data);
-
+    //If there is more than one busy hour
     if (Object.keys(data).length > 0) {
+
+        //Iterating over the hours
         for (var hour in data) {
+            //Retrieve next hour
             if (!data.hasOwnProperty(hour)) {
                 continue;
             }
-
             var certificates_in_hour = data[hour];
 
+            //If there is more than one flight in that hour
             if (certificates_in_hour.length != 0) {
-                //console.log($(table_selector + " tr:last"))
-                $(table_selector).append(
-                    "<tr class='hour-row'>" +
-                    "   <td>" + hour + ":00</td>" +
-                    "   <td style='padding: 10px 20px;'>" +
-                    "       <div class='row certificates-in-hour-" + hour + "'></div>" +
-                    "   </td>" +
-                    "</tr>"
-                );
+                //Creating new hour row in table
+                $(table_selector).append(getHourRow(hour));
 
+                //Iterating over the flights in this hour and rendering them
                 certificates_in_hour.forEach(function(item, i, arr){
-                    $(table_selector + " .certificates-in-hour-"+hour).append(
-                        "<a href='#'>" +
-                        "   <div class='col-md-3'>" +
-                        "       <b>" + item.id + "</b>" + " | " + item.flight_type +
-                        "   </div>" +
-                        "</a>"
-                    );
+                    $(table_selector + " .certificates-in-hour-"+hour).append(certificateToStr(item));
                 });
-                /*$(table_selector + " tr:last").after(
-                 "<tr>" +
-                 "   <td>" + hour + ":00</td>" +
-                 "   <td>sa "
-                 );
-                 certificates_in_hour.forEach(function (item) {
-                 console.log(item.id);
-                 $(table_selector + " td:last").html(
-                 item.id + " | " + item.flight_type + "&nbsp;&nbsp;"
-                 )
-                 });
-                 $(table_selector + " td:last").after(
-                 "</td></tr>"
-                 );*/
             }
         }
     }
+    //Otherwise showing the string telling the day is empty
     else {
         $(table_selector + " .day-empty").removeClass("hidden");
     }
