22fdfe24bdcb53844e8b5782268d0c6d7a367e51
Nef1k <nef1k@outlook.com>
2016-09-23 08:59:12 +0300

    Certificates data during creation was retrieved

--- web/js/admin_script.js
+++ web/js/admin_script.js
@@ -2,11 +2,17 @@
  * Created by ASUS on 20.09.2016.
  */
 
+var yesNoDialog = new YesNoDialog;
+
+function isDefined(varible){
+    return (typeof varible !== "undefined");
+}
+
 function createModalNoBtn(event){
-    closeYesNoModal();
+    yesNoDialog.close();
 }
 function createModalYesBtn(event) {
-    closeYesNoModal();
+    yesNoDialog.close();
 }
 
 function getCertificateListCaption(event){
@@ -19,8 +25,52 @@ function getCertificatesCaption(event){
 
 }
 
+function isInvalid(value){
+    return isNaN(value) || value == "";
+}
+
+function parseCertificateList(){
+    var ID_Certificate = $("[name='ID_Certificate']").val();
+    var range = {
+        from: $("[name='range_from']").val(),
+        to: $("[name='range_to']").val()
+    };
+    var amount = {
+        from: $("[name='amount_from']").val(),
+        count: $("[name='amount_count']").val()
+    };
+
+    console.log(isInvalid(ID_Certificate));
+
+    if (!isInvalid(ID_Certificate)){
+        return {
+            type: "single",
+            certificates: [ID_Certificate]
+        };
+    }
+
+    if (!isInvalid(range.from) && !isInvalid(range.to)){
+        return {
+            "type": "range",
+            "certificates": Array(range.to - range.from + 1).map(function(value, index, certificates){
+                return range.from + index;
+            })
+        }
+    }
+
+    if (!isInvalid(amount.from) && !isInvalid(amount.count)){
+        return {
+            "type": "amount",
+            "certificates": Array(amount.count).map(function(value, index, certificates){
+                return amount.from + index;
+            })
+        };
+    }
+}
+
 function createBtnClick(){
-    var yesNoDialog = new YesNoDialog();
+    console.log(parseCertificateList().certificates);
+    yesNoDialog.setModalSelector("#yes-no-modal");
     yesNoDialog.show({
         caption: "Создание сертификатов",
         message: "Будут созданы сертификаты %certificates%. Вы уверены?",
@@ -28,8 +78,8 @@ function createBtnClick(){
         yes_caption: "Да",
         no_caption: "Нет",
 
-        yesHandler: createModalYesBtn,
-        noHandler: createModalNoBtn
+        yes_handler: createModalYesBtn,
+        no_handler: createModalNoBtn
     });
 }
 
