9da5451ed021db80db51312d3814d44c323725bc
RedSkyDragon <redxsky59@gmail.com>
2016-09-23 17:30:54 +0300

    Added finding by certificate ID.

--- web/js/create_calendar.js
+++ web/js/create_calendar.js
@@ -19,7 +19,9 @@ function getHourRow(hour){
 function showModal(date){
     //Showing modal window with per-day schedule table
     $("#chosen_date").html(date.format("DD.MM.YYYY"));
+    $(".current-date").html(date.format("YYYY-MM-DD"));
     $("#time_table").find(".hour-row").remove();
+    $("#found_certs").find(".cert_row").remove();
     $(".day-empty").addClass("hidden");
     $("#myModal").modal("show");
 
@@ -66,14 +68,93 @@ function getTimeTableData(date){
 }
 
 function getCertRow(cert) {
-    return "<tr><td><a href=/'{{ path('" + cert.cert_link + "') }}'>"+cert.ID_Sertificate+"</td><td>"+
-            cert.name + " " + cert.last_name + "</a></td><td></td></tr>"
+    return "<tr class='cert_row'>" +
+        "   <td>" +
+        "       <a href='" + cert.cert_link + "'>"+cert.ID_Sertificate+"</a>" +
+        "   </td>" +
+        "   <td>"+
+                cert.name + " " + cert.last_name + "" +
+        "   </td>" +
+        "   <td>" +
+        "       <button class='btn btn-xs btn-default' data-toggle='collapse' data-target='#" + cert.ID_Sertificate + "'>" +
+        "           <span class='glyphicon glyphicon-triangle-bottom'></span>" +
+        "       </button>" +
+        "   </td>" +
+        "   </tr>" +
+        "   <tr id='"+cert.ID_Sertificate+"' class='collapse cert_row'>" +
+        "    <td colspan='3'>" +
+        "           <div class='input-group'>" +
+        "               <input type='text' class='form-control' placeholder='Часы' id='hours-"+cert.ID_Sertificate+"'>" +
+        "               <span class='input-group-addon'><b>:</b></span>" +
+        "               <input type='text' class='form-control' placeholder='Минуты' id='minutes-"+cert.ID_Sertificate+"'>" +
+        "               <span class='input-group-btn'>"+
+        "                   <button class='btn btn-default set-use-time-btn' type='button' data-certificate-id='"+ cert.ID_Sertificate +"'>Ok</button>" +
+        "               </span>" +
+        "           </div>" +
+        "   </td>" +
+        "   </tr>"
+}
+
+function onYesSetTime(event){
+    var modal_window = event.data;
+    var date = moment(modal_window.data.date).unix();
+    var certificate_id = JSON.stringify(modal_window.data.certificate_id);
+    var fieldnames = JSON.stringify(["use_time"]);
+    var fieldvalues = JSON.stringify([date]);
+    modal_window.showLoader();
+    //TODO: Make post query to set use_time to certificate
+    jQuery.post("/certificate/edit", {id : certificate_id, field_names : fieldnames, field_values: fieldvalues}, function (data) {
+        console.log(data)
+    });
+    modal_window.hideLoader();
+    modal_window.close();
+    console.log(date, certificate_id);
+}
+
+function onNoSetTime(event){
+
+}
+
+function onSetUseTime(event){
+    var btn = $(this);
+
+    var certificate_id = $(this).data("certificate-id");
+    if (certificate_id == "undefined"){
+        return false;
+    }
+
+    var hours = $("#hours-"+certificate_id).val();
+    var minutes = $("#minutes-"+certificate_id).val();
+    var seconds = "00";
+    var date=$(".current-date").html() + " " + [hours, minutes, seconds].join(":");
+    //ToDo check validate time
+    var dialog = new YesNoDialog();
+    dialog.setModalSelector("#yes-no-modal");
+    dialog.show({
+        caption: "Установка времеи",
+        message: date,
+
+        yes_caption: "Установить",
+        no_caption: "Отмена",
+
+        data: {
+            "certificate_id": certificate_id,
+            "date": date
+        },
+
+        yes_handler: onYesSetTime,
+        no_handler: onNoSetTime
+    });
+
+    return true;
 }
 
 function fillCertsListWithData(table_selector,data){
     data.forEach(function(item) {
        $(table_selector).append(getCertRow(item))
-    })
+    });
+    $(".set-use-time-btn").click(onSetUseTime);
+    $("#find_cert").click(data, findByIDCertificate);
 }
 
 function findCertificates() {
@@ -85,6 +166,24 @@ function findCertificates() {
         fillCertsListWithData("#found_certs",data)
     })
 
+}
+
+function findByIDCertificate(data){
+    var id = $("#cert_id_for_watching").val();
+    var found = false;
+    var certificate_list = data.data;
+
+    certificate_list.forEach(function (item) {
+        if (item.ID_Sertificate == id){
+            $("#found_certs").find(".cert_row").remove();
+            $("#found_certs").append(getCertRow(item));
+            found = true;
+        }
+    });
+    if (!found){
+        $("#found_certs").find(".cert_row").remove();
+        $("#found_certs").append("<tr class='cert_row'><td colspan='3'>Сертификат не найдет</td></tr>");
+    }
 }
 
 $(document).ajaxError(function(event, jqXHR, ajaxSettings, message){
